343c07c5946fd81dead3848caa602bd509efec47
Wian Stipp <stippwian@gmail.com>
2023-06-11 21:51:22 -0700

    init pico code

---
+++ pico/build/CMakeFiles/test.dir/Users/wianstipp/Desktop/projects/pico/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp.obj.d
@@ -0,0 +1,25 @@
+CMakeFiles/test.dir/Users/wianstipp/Desktop/projects/pico/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp.obj: \
+ /Users/wianstipp/Desktop/projects/pico/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/c++/11.3.1/cstdlib \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/c++/11.3.1/arm-none-eabi/thumb/v6-m/nofp/bits/c++config.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/c++/11.3.1/arm-none-eabi/thumb/v6-m/nofp/bits/os_defines.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/c++/11.3.1/arm-none-eabi/thumb/v6-m/nofp/bits/cpu_defines.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/c++/11.3.1/pstl/pstl_config.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/stdlib.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/machine/ieeefp.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/_ansi.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/newlib.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/sys/config.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/lib/gcc/arm-none-eabi/11.3.1/include/stddef.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/sys/reent.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/_ansi.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/sys/_types.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/machine/_types.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/sys/lock.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/sys/cdefs.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/machine/stdlib.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/alloca.h \
+ /Applications/ArmGNUToolchain/11.3.rel1/arm-none-eabi/arm-none-eabi/include/c++/11.3.1/bits/std_abs.h
